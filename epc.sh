22f987762bb2f8d7c8afc97a6842b98b4d5c340b
sharonlwl128 <44594785+sharonlwl128@users.noreply.github.com>
2019-03-19 00:37:37 -0400

    Update epc.sh

--- epc.sh
+++ epc.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
-echo "testing"
+
+script_name=${BASH_SOURCE[0]}
+
+for pid in $(pidof -x $script_name); do
+  if [ $pid != $$ ]; then
+    kill -9 $pid
+  fi
+done
+
 ping -c 5 10.11.5.201
 cd srsepc
 sudo srsepc epc.conf
