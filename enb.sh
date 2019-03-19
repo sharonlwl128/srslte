615bdf683c4baab35a470255b214997ea35d2f9e
sharonlwl128 <44594785+sharonlwl128@users.noreply.github.com>
2019-03-19 00:38:16 -0400

    Update enb.sh

--- enb.sh
+++ enb.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
-echo "testing"
+script_name=${BASH_SOURCE[0]}
+
+for pid in $(pidof -x $script_name); do
+  if [ $pid != $$ ]; then
+    kill -9 $pid
+  fi
+done
+
 ping -c 5 10.11.5.201
+ping -c 5 10.11.5.203
+
 cd srsenb
 srsenb enb.conf
