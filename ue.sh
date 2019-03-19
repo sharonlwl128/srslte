c769426841e7f56199395ea111f87723d1ab4d11
sharonlwl128 <44594785+sharonlwl128@users.noreply.github.com>
2019-03-19 00:39:04 -0400

    Update ue.sh

--- ue.sh
+++ ue.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
-echo "testing"
-ping -c 10 10.11.5.201
+script_name=${BASH_SOURCE[0]}
+
+for pid in $(pidof -x $script_name); do
+  if [ $pid != $$ ]; then
+    kill -9 $pid
+  fi
+done
+
+ping -c 5 10.11.5.201
+ping -c 5 10.11.5.203
+
 cd srsue
 sudo srsue ue.conf
